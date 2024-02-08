b6dbea158631ec2f417c0b1a7315edc1bb49a580
Luke Albert <128676520+Lukeaalbert@users.noreply.github.com>
2024-02-07 21:25:45 -0800

    Create clothing.h

---
+++ clothing.h
@@ -0,0 +1,28 @@
+#ifndef CLOTHING_H
+#define CLOTHING_H
+#include <iostream>
+#include <string>
+#include <set>
+#include <vector>
+#include <algorithm>
+
+#include "product.h"
+
+using namespace std;
+
+class Clothing: public Product 
+{
+  public: 
+    Clothing(const std::string category, const std::string name, double price, int qty, const std::string Sizee, const std::string Brand);
+    virtual ~Clothing();
+
+    virtual std::set<std::string> keywords() const;
+    virtual std::string displayString() const;
+    virtual void dump(std::ostream& os) const;
+
+  private:
+    std::string Size_;
+    std::string Brand_;
+};
+
+#endif
