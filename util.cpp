61f39e70bd1047af21bab488233ee00c48be31e9
Luke Albert <128676520+Lukeaalbert@users.noreply.github.com>
2024-02-07 21:34:29 -0800

    Create util.cpp

---
+++ util.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <sstream>
+#include <cctype>
+#include <algorithm>
+#include "util.h"
+
+using namespace std;
+std::string convToLower(std::string src)
+{
+    std::transform(src.begin(), src.end(), src.begin(), ::tolower);
+    return src;
+}
+
+std::set<std::string> parseStringToWords(string rawWords)
+{
+  std::set<string> hitWords; 
+
+  std::stringstream ss; 
+
+  ss << rawWords;
+  string tmp;
+
+  while(ss >> tmp)
+  {
+    int count = 0;
+    string toRet;
+
+    for (unsigned int i = 0; i < tmp.size(); ++i)
+    {
+      if (ispunct(tmp[i]))
+      {
+        if (count >= 2)
+        {
+          toRet = convToLower(toRet);
+          hitWords.insert(trim(toRet));
+        }
+        count = 0;
+        toRet.clear();
+      }
+
+      else
+      {
+        ++count;
+        toRet += tmp[i];
+      }
+    }
+
+    if (count >= 2)
+    {
+      toRet = convToLower(toRet);
+      hitWords.insert(trim(toRet));
+    }
+  }
+
+  return hitWords;
+}
+
+// Used from http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+// trim from start
+std::string &ltrim(std::string &s) {
+    s.erase(s.begin(), 
+	    std::find_if(s.begin(), 
+			 s.end(), 
+			 std::not1(std::ptr_fun<int, int>(std::isspace))));
+    return s;
+}
+
+// trim from end
+std::string &rtrim(std::string &s) {
+    s.erase(
+	    std::find_if(s.rbegin(), 
+			 s.rend(), 
+			 std::not1(std::ptr_fun<int, int>(std::isspace))).base(), 
+	    s.end());
+    return s;
+}
+
+// trim from both ends
+std::string &trim(std::string &s) {
+    return ltrim(rtrim(s));
+}
